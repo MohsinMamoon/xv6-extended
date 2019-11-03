2b2bacbef701556da78c520fd5a22b0b82b028a7
Mohsin Mamoon <mohsin.mamoon@students.iiit.ac.in>
2019-11-03 22:49:37 +0530

    added initial wait

--- proc.h
+++ proc.h
@@ -49,6 +49,9 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  uint ctime;                   // ticks at the time of process creation
+  uint etime;                   // Process end time (ticks at end time - ctime)
+  uint rtime;                   // Process rtime
 };
 
 // Process memory is laid out contiguously, low addresses first:
@@ -56,3 +59,5 @@ struct proc {
 //   original data and bss
 //   fixed-size stack
 //   expandable heap
+
+void update_rtime(void);
---
+++ waittest.c
@@ -0,0 +1,58 @@
+// Test that fork fails gracefully.
+// Tiny executable so that the limit can be filling the proc table.
+
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+#define N  1000
+/*
+void
+printf(int fd, char *s, ...)
+{
+  write(fd, s, strlen(s));
+}
+*/
+
+void
+foo()
+{
+  volatile int i, j = 0;
+  for (i=0;i<2000;i++) {
+     printf(2, "wait test %d\n",i);
+    j++;
+  }
+  sleep(20);
+  for (i=0;i<2000;i++) {
+     printf(2, "wait test %d\n",i);
+    j++;
+  }
+  printf(2, "%d", j);
+}
+
+void
+waittest(void)
+{
+  int wTime;
+  int rTime;
+  int pid;
+  printf(1, "wait test\n");
+
+
+    pid = fork();
+    if(pid == 0)
+    {
+      foo();
+      exit();      
+    }
+    waitx(&rTime,&wTime);
+     printf(1, "hi \n");
+    printf(1, "wTime: %d rTime: %d \n",wTime,rTime);
+
+}
+int
+main(void)
+{
+  waittest();
+  exit();
+}
